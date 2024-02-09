38e69ef13b38df7d47d94836cde8986afe127f46
Matheus Link <linksauermths2203@gmail.com>
2024-02-08 22:19:59 -0300

    Criando pastas de componentes, jsx e styles correspondentes

---
+++ src/Componentes/Header/index.jsx
@@ -0,0 +1,17 @@
+import './style.scss'
+
+const index = () => {
+  return (
+    <header className="header-portfolio">
+      <nav>
+        <a href="#">Início</a>
+        <a href="#">Sobre Mim</a>
+        <a href="#">Projetos</a>
+        <a href="#">Habilidades</a>
+        <a href="#">Certificações</a>
+      </nav>
+    </header>
+  );
+};
+
+export default index;
