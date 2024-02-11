9f8721d32f85a8996232b9040ac0f3847a23aa3f
Matheus Link <linksauermths2203@gmail.com>
2024-02-11 20:35:35 -0300

    renderizando e estilizando tela de habilidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Habilidades from "./Componentes/Habilidades"
 import Header from "./Componentes/Header"
 
 function App() {
  return(
   <main className="App">
     <Header/>
+    <Habilidades/>
   </main>
  )
 }
--- src/Componentes/Habilidades/index.jsx
+++ src/Componentes/Habilidades/index.jsx
@@ -1,1 +1,25 @@
 import "./style.scss";
+
+import minhashabilidades from "../../data/Habilidades/habilidades.json";
+
+const Habilidades = () => {
+  return (
+    <main id="pagina-habilidades">
+      <h1 className="nome-pagina">Minhas Habilidades</h1>
+      <article className="lista-habilidades">
+        {minhashabilidades.map((habilidade, index) => {
+          return (
+            <section key={index} className="habilidade">
+              <h5>{habilidade.habilidade}</h5>
+              <div>
+                <img src={habilidade.imagem} alt={habilidade.habilidade} />
+              </div>
+            </section>
+          );
+        })}
+      </article>
+    </main>
+  );
+};
+
+export default Habilidades;
