7df977107e7510ce2c07013bf652d0a642822659
Matheus Link <linksauermths2203@gmail.com>
2024-02-14 21:38:46 -0300

    Tentando corrigir imagens nao renderizadas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import json from '@rollup/plugin-json';
+import image from '@rollup/plugin-image';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    json(),
+    image(),
+  ],
   base: "/portfolio-matheus_link/",
 });
