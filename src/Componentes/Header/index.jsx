4bdc3e59edc71cb9ee81e5a5d88050153e4c38ab
Matheus Link <linksauermths2203@gmail.com>
2024-02-17 23:39:46 -0300

    Deixando header responsivo, em telas menores será aberto um menu hamburguer no lugar dos links

--- src/Componentes/Header/index.jsx
+++ src/Componentes/Header/index.jsx
@@ -1,19 +1,45 @@
-import './style.scss'
+import { useState } from "react";
+import "./style.scss";
+
+import menuIcon from '/public/IconMenu/menu.svg'
 
 const Header = () => {
   const scrollIntoView = (id) => {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
   };
 
+  const [linksAbertos, setLinksAbertos] = useState(false);
+  const [classeMenu, setClasseMenu] = useState("mobile-menu");
+
+  const handleShowLinks = () => {
+    setLinksAbertos(!linksAbertos);
+  };
+
   return (
     <header className="header-portfolio">
+      <h1 className="logo">MatheusLink Portfolio</h1>
       <nav>
-        <a onClick={() => scrollIntoView("inicio")}>Início</a>
-        <a onClick={() => scrollIntoView("sobreMim")}>Sobre Mim</a>
-        <a onClick={() => scrollIntoView("projetos")}>Projetos</a>
-        <a onClick={() => scrollIntoView("habilidades")}>Habilidades</a>
-        <a onClick={() => scrollIntoView("certificados")}>Certificações</a>
+        <section
+          className={classeMenu}
+          onClick={(e) => {
+            e.preventDefault();
+            handleShowLinks();
+          }}
+        >
+          <img src={menuIcon} alt="" />
+        </section>
+
+        <ul
+          className={`links ${linksAbertos ? "menu-aberto" : "menu-fechado"}`}
+        >
+          <a onClick={() => scrollIntoView("inicio")}>Início</a>
+          <a onClick={() => scrollIntoView("sobreMim")}>Sobre Mim</a>
+          <a onClick={() => scrollIntoView("projetos")}>Projetos</a>
+          <a onClick={() => scrollIntoView("habilidades")}>Habilidades</a>
+          <a onClick={() => scrollIntoView("certificados")}>Certificações</a>
+        </ul>
       </nav>
+      <main></main>
     </header>
   );
 };
